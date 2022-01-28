7498f913c7ab81ae12bb321e50a4edcf4d3482b3
jay bhatt <202112108@daiict.ac.in>
2022-01-28 22:56:46 +0530

    Add Task which To be completed by 28th January, 2022
    Created Event-Dashboard

--- EventManagerWebApp/event/urls.py
+++ EventManagerWebApp/event/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('events', views.events, name="events"),
+    path('eventDashboard', views.eventDashboard, name="eventDashboard"),
+    path('participantDetails/<str:name>/', views.participantDetails, name="participantDetails"),
     path('eventRegistration', views.eventRegistration, name="eventRegistration"),
     path('participantRegistration', views.participantRegistration, name="participantRegistration"),
     path('about', views.about, name="about"),
