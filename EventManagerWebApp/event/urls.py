1eabff533cd7920ec920a1e85d32fe1fee9037e2
jay bhatt <202112108@daiict.ac.in>
2022-01-31 23:53:24 +0530

    Add Last minutes changes and **Final Submission**

--- EventManagerWebApp/event/urls.py
+++ EventManagerWebApp/event/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('eventRegistration', views.eventRegistration, name="eventRegistration"),
     path('participantRegistration', views.participantRegistration, name="participantRegistration"),
     path('about', views.about, name="about"),
-    path('contact', views.contact, name="contact")
+    path('contact', views.contact, name="contact"),
+    path('logout', views.logout, name="logout")
 ]
